2cd70b54e9fc1e7e1e6cc6d029fb01639c4aa445
futuretea <futuretea1024@gmail.com>
2023-12-24 17:49:39 +0800

    Update
    Signed-off-by: futuretea <futuretea1024@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "tailwindcss/tailwind.css";
 
 function App() {
+  const [power, setPower] = useState(100); // in watts
+  const [hours, setHours] = useState(8);
+  const [rate, setRate] = useState(0.6); // in RMB per kWh
+  const [result, setResult] = useState(null);
+
+  const calculateCosts = () => {
+    const daily = (power / 1000) * hours * rate;
+    const weekly = daily * 7;
+    const monthly = daily * 30;
+    const yearly = daily * 365;
+    setResult({ daily, weekly, monthly, yearly });
+  };
+
+  const allFieldsFilled = power && hours && rate;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 px-4">
+      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <h1 className="block text-gray-700 text-xl font-bold mb-2">
+          Electricity Cost Calculator
+        </h1>
+
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="power"
+          >
+            Device Power (W)
+          </label>
+          <input
+            id="power"
+            type="number"
+            value={power}
+            onChange={(e) => setPower(e.target.value)}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="hours"
+          >
+            Daily Usage (Hours)
+          </label>
+          <input
+            id="hours"
+            type="number"
+            value={hours}
+            onChange={(e) => setHours(e.target.value)}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="rate"
+          >
+            Electricity Rate (RMB/kWh)
+          </label>
+          <input
+            id="rate"
+            type="number"
+            value={rate}
+            onChange={(e) => setRate(e.target.value)}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+
+        <div className="flex items-center justify-between">
+          <button
+            className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${
+              !allFieldsFilled && "opacity-50 cursor-not-allowed"
+            }`}
+            onClick={calculateCosts}
+            disabled={!allFieldsFilled}
+          >
+            Calculate
+          </button>
+        </div>
+      </div>
+
+      {result && (
+        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <h2 className="block text-gray-700 text-lg font-bold mb-2">
+            Results
+          </h2>
+          <table className="table-fixed w-full">
+            <thead>
+              <tr className="bg-gray-200">
+                <th className="px-4 py-2">Daily</th>
+                <th className="px-4 py-2">Weekly</th>
+                <th className="px-4 py-2">Monthly</th>
+                <th className="px-4 py-2">Yearly</th>
+              </tr>
+            </thead>
+            <tbody className="text-center">
+              <tr>
+                <td className="border px-4 py-2">{result.daily.toFixed(2)}</td>
+                <td className="border px-4 py-2">{result.weekly.toFixed(2)}</td>
+                <td className="border px-4 py-2">
+                  {result.monthly.toFixed(2)}
+                </td>
+                <td className="border px-4 py-2">{result.yearly.toFixed(2)}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import React from "react";
 
-test('renders learn react link', () => {
+test('allows the user to calculate costs', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // Find input fields and button
+  const powerInput = screen.getByLabelText(/device power/i);
+  const hoursInput = screen.getByLabelText(/daily usage/i);
+  const rateInput = screen.getByLabelText(/electricity rate/i);
+  const calculateButton = screen.getByRole('button', { name: /calculate/i });
+
+  // Fill out the form
+  fireEvent.change(powerInput, { target: { value: '100' } });
+  fireEvent.change(hoursInput, { target: { value: '8' } });
+  fireEvent.change(rateInput, { target: { value: '0.6' } });
+
+  // Click the calculate button
+  fireEvent.click(calculateButton);
+
+  // Check for the results
+  const dailyResult = screen.getByText(/yearly/i);
+  expect(dailyResult).toBeInTheDocument();
+  // You could add more specific assertions here to check the calculated values
 });
